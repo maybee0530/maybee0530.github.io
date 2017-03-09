6b36c2c2f81e9b3574bc9743d354636dff0d8ed8
Seoyoungyun <yuns781@newschool.edu>
2017-03-09 02:38:53 -0500

    qq

--- arrays/translator.js
+++ arrays/translator.js
@@ -1,10 +1,18 @@
 var submitbutton = document.querySelector("#textSubmit");
 var textfield = document.querySelector("#enteredText");
 
-submitbutton.addEventListener("click", report);
+submitbutton.addEventListener("click", do_stuff);
 
-function report(e) {
-    e.preventDefault();
-    var word = String(textfield.value);
-    console.log(word);
+function do_stuff() {
+	var input_string = textfield.value;
+	var the_four_letter = [];
+	the_four_letter.push
+	console.log(inout_string);
+	console.log (inout_string.charAt(0));
 }
+
+//function report(e) {
+ //  e.preventDefault();
+   // var word = String(textfield.value);
+   // console.log(word);
+//}
