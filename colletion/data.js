c473df683f0802882d0150d46cde29c49d79aaa0
Seoyoungyun <yuns781@newschool.edu>
2017-05-02 17:24:48 -0400

    1

---
+++ colletion/data.js
@@ -0,0 +1,18 @@
+var trashcollect= [
+
+{
+	"date": "april 18", 
+	"size": "small",
+	"color": "",
+	"image_source": "images/trash1.jpg"
+}
+{
+	"date": "april 19", 
+	"size": "medium"
+}
+{
+	"date": "april 20", 
+	"size": "small"
+}
+
+];
