adb3577985a652523eb7c08b5ea4eeb11f314920
Seoyoungyun <yuns781@newschool.edu>
2017-05-11 16:26:49 -0400

    final

---
+++ collection /js/data.js
@@ -0,0 +1,196 @@
+var trashcollect= [
+
+{
+	"date": "april 14", 
+	"type": "disposable",
+	"color": "white",
+	"image_source": "day1.png"
+},
+{
+	"date": "april 15", 
+	"type": "biodegradable",
+	"color": "red",
+	"image_source": "day2.png"
+},
+{
+	"date": "april 16", 
+	"type": "biodegradable",
+	"color": "yellow",
+	"image_source": "day3.png"
+},
+{
+	"date": "april 17", 
+	"type": "disposable",
+	"color": "brown",
+	"image_source": "day4.png"
+
+},
+{
+	"date": "april 18", 
+	"type": "recycable",
+	"color": "white",
+	"image_source": "day5.png"
+
+},
+{
+	"date": "april 19", 
+	"type": "disposable",
+	"color": "white",
+	"image_source": "day6.png"
+
+},
+{
+	"date": "april 20", 
+	"type": "recycable",
+	"color": "white",
+	"image_source": "day7.png"
+
+},
+{
+	"date": "april 21", 
+	"type": "disposable",
+	"color": "brown",
+	"image_source": "day8.png"
+},
+{
+	"date": "april 22", 
+	"type": "recycable",
+	"color": "red",
+	"image_source": "day9.png"
+
+},
+{
+	"date": "april 23", 
+	"type": "recycable",
+	"color": "white",
+	"image_source": "day10.png"
+
+},
+{
+	"date": "april 24", 
+	"type": "recycable",
+	"color": "white",
+	"image_source": "day11.png"
+
+},
+{
+	"date": "april 25", 
+	"type": "recycable",
+	"color": "green",
+	"image_source": "day12.png"
+
+},
+{
+	"date": "april 26", 
+	"type": "disposable",
+	"color": "yellow",
+	"image_source": "day13.png"
+
+},
+{
+	"date": "april 26", 
+	"type": "recycable",
+	"color": "white",
+	"image_source": "day14.png"
+
+},
+{
+	"date": "april 27", 
+	"type": "recycable",
+	"color": "green",
+	"image_source": "day15.png"
+
+},
+{
+	"date": "april 28", 
+	"type": "recycable",
+	"color": "red",
+	"image_source": "day16.png"
+},
+{
+	"date": "april 29", 
+	"type": "disposable",
+	"color": "brown",
+	"image_source": "day17.png"
+},
+{
+	"date": "april 30", 
+	"type": "biodegradable",
+	"color": "blue",
+	"image_source": "day18.png"
+},
+{
+	"date": "may 01", 
+	"type": "recycable",
+	"color": "white",
+	"image_source": "day19.png"
+},
+{
+	"date": "May 02", 
+	"type": "disposable",
+	"color": "brown",
+	"image_source": "day20.png"
+},
+{
+	"date": "May 03", 
+	"type": "disposable",
+	"color": "brown",
+	"image_source": "day21.png"
+},
+{
+	"date": "May 04", 
+	"type": "recycable",
+	"color": "blue",
+	"image_source": "day22.png"
+},
+{
+	"date": "May 05", 
+	"type": "recycable",
+	"color": "green",
+	"image_source": "day23.png"
+},
+{
+	"date": "May 06", 
+	"type": "disposable",
+	"color": "red",
+	"image_source": "day24.png"
+},
+{
+	"date": "May 07", 
+	"type": "recycable",
+	"color": "yellow",
+	"image_source": "day25.png"
+},
+{
+	"date": "May 08", 
+	"type": "recycable",
+	"color": "red",
+	"image_source": "day26.png"
+},
+{
+	"date": "May 09", 
+	"type": "biodegradable",
+	"color": "green",
+	"image_source": "day27.png"
+},
+{
+	"date": "May 10", 
+	"type": "recycable",
+	"color": "green",
+	"image_source": "day28.png"
+},
+{
+	"date": "May 11", 
+	"type": "recycable",
+	"color": "blue",
+	"image_source": "day29.png"
+},
+{
+	"date": "May 12", 
+	"type": "disposable",
+	"color": "red",
+	"image_source": "day30.png"
+},
+
+
+];
